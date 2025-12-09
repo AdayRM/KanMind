9da45de5e7dc8ae54d3321698ff04e5c4c1615fa
Aday Reyes <adayreyesdev@gmail.com>
2025-12-09 19:59:50 +0100

    feat: add task-related views and update permissions for BoardViewSet

--- kanban_app/api/urls.py
+++ kanban_app/api/urls.py
@@ -1,8 +1,25 @@
-from django.urls import path
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from kanban_app.api.views import BoardViewSet
+from kanban_app.api.views import (
+    BoardViewSet,
+    EmailCheckView,
+    TasksAssignedListViewSet,
+    TasksCreateRetrieveUpdateDestroyViewSet,
+    TasksReviewingListViewSet,
+)
 
 router = DefaultRouter()
 router.register(r"boards", BoardViewSet, basename="board")
-urlpatterns = router.urls
+router.register(r"tasks", TasksCreateRetrieveUpdateDestroyViewSet, basename="tasks")
+router.register(
+    r"tasks/assigned-to-me", TasksAssignedListViewSet, basename="tasks_assigned"
+)
+router.register(
+    r"tasks/reviewing", TasksReviewingListViewSet, basename="tasks_reviewing"
+)
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path("email-check/", EmailCheckView.as_view(), name="email_check"),
+]
--- kanban_app/api/views.py
+++ kanban_app/api/views.py
@@ -1,13 +1,26 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
+
 from django.db.models import Q
 from rest_framework.permissions import IsAuthenticated
-from kanban_app.api.permissions import IsBoardOwner, IsBoardOwnerOrMember
+from kanban_app.api.serializers import AccountSerializer
+from auth_app.models import Account
+from kanban_app.api.permissions import (
+    IsBoardOwner,
+    IsBoardOwnerOrMember,
+    IsTaskBoardOwnerOrMember,
+    IsTaskOrBoardOwner,
+)
 from kanban_app.api.serializers import (
     BoardDetailSerializer,
     BoardListSerializer,
     BoardUpdateSerializer,
+    TaskSerializer,
 )
-from kanban_app.models import Board
+from kanban_app.models import Board, Task
+from django.shortcuts import get_object_or_404
 
 
 """ 
@@ -17,8 +30,10 @@ Obj permissions will only apply to retrieve,update or delete. For lists we use g
 
 class BoardViewSet(viewsets.ModelViewSet):
     def get_permissions(self):
+        if self.action == "create":
+            return [IsAuthenticated()]
         if self.action == "destroy":
-            return [IsBoardOwner()]
+            return [IsAuthenticated(), IsBoardOwner()]
         return [IsAuthenticated(), IsBoardOwnerOrMember()]
 
     def get_queryset(self):
@@ -31,3 +46,49 @@ class BoardViewSet(viewsets.ModelViewSet):
         if self.action == "partial_update":
             return BoardUpdateSerializer
         return BoardDetailSerializer
+
+
+class TasksAssignedListViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    def get_queryset(self):
+        account = self.request.user.account
+        return Task.objects.filter(assignee=account)
+
+    serializer_class = TaskSerializer
+
+
+class TasksReviewingListViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    def get_queryset(self):
+        account = self.request.user.account
+        return Task.objects.filter(reviewer=account)
+
+    serializer_class = TaskSerializer
+
+
+class TasksCreateRetrieveUpdateDestroyViewSet(
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Task.objects.all()
+
+    def get_permissions(self):
+        if self.action == "destroy":
+            return [IsAuthenticated(), IsTaskOrBoardOwner()]
+
+        return [IsAuthenticated(), IsTaskBoardOwnerOrMember()]
+
+    serializer_class = TaskSerializer
+
+
+class EmailCheckView(APIView):
+    def get(self, request):
+        email = request.query_params.get("email")
+
+        if email is None:
+            raise ValidationError({"email": "Email is a required param"})
+
+        account = get_object_or_404(Account, user__email=email)
+
+        return Response(AccountSerializer(account).data)
