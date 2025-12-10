8f361d4c3e0072690e1962148010421c3b2eed32
Aday Reyes <adayreyesdev@gmail.com>
2025-12-10 19:08:44 +0100

    feat: update URL routing for task comments and assigned/reviewing tasks

--- kanban_app/api/urls.py
+++ kanban_app/api/urls.py
@@ -4,22 +4,33 @@ from rest_framework.routers import DefaultRouter
 from kanban_app.api.views import (
     BoardViewSet,
     EmailCheckView,
-    TasksAssignedListViewSet,
+    TaskCommentListCreateView,
+    TasksAssignedListView,
     TasksCreateRetrieveUpdateDestroyViewSet,
-    TasksReviewingListViewSet,
+    TasksReviewingListView,
+    TaskCommentDestroyView,
 )
 
 router = DefaultRouter()
 router.register(r"boards", BoardViewSet, basename="board")
 router.register(r"tasks", TasksCreateRetrieveUpdateDestroyViewSet, basename="tasks")
-router.register(
-    r"tasks/assigned-to-me", TasksAssignedListViewSet, basename="tasks_assigned"
-)
-router.register(
-    r"tasks/reviewing", TasksReviewingListViewSet, basename="tasks_reviewing"
-)
+
 
 urlpatterns = [
-    path("", include(router.urls)),
     path("email-check/", EmailCheckView.as_view(), name="email_check"),
+    path(
+        "tasks/assigned-to-me/", TasksAssignedListView.as_view(), name="tasks_assigned"
+    ),
+    path("tasks/reviewing/", TasksReviewingListView.as_view(), name="tasks_reviewing"),
+    path(
+        "tasks/<int:task_id>/comments/",
+        TaskCommentListCreateView.as_view(),
+        name="task_comments",
+    ),
+    path(
+        "tasks/<int:task_id>/comments/<int:pk>/",
+        TaskCommentDestroyView.as_view(),
+        name="task_comments-detail",
+    ),
+    path("", include(router.urls)),
 ]
